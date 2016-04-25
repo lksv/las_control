bb4ba3ddb510781c35536650bb2e2e2c8bb40dc5
Lukas Svoboda <lukas.svoboda@gmail.com>
2016-04-25 16:39:36 +0200

    UX-fix: v Documents#show zobrazovat Katastralni uzemi zelene, popover s max-height s scrollovanim

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -5,4 +5,29 @@ module DocumentsHelper
     end
     snippets.sort_by!(&:first)
   end
+
+  def link_to_if_address_cache(text, url, address_block)
+    ku = address_block.events.all? { |t| t.shape_with_definition_point&.source_type == 'KatastralniUzemi' }
+
+    # do not show popover for Katastralni Uzemi
+    if ku
+      content_tag(:span, text, class: 'ku')
+    else
+      # TODO: for showing unknown addresses with custom style
+      # unknown = address_block.events.any? { |t| t.address_cache.blank? || t.shape_with_definition_point.nil? }
+      event_classes = address_block.events.map { |e| "event-#{e.id}" }
+      link_to(
+        text,
+        url,
+        class: event_classes.join(' '), # + (unknown ? ' unknown-place' : ''),
+        id: "addressBlock-#{address_block.id}",
+        title: "<a href=\"#addressBlock-#{address_block.id}\">Adresy <i class=\"fa fa-link\"></a>",
+        data: {
+          toggle: 'popover',
+          content: '<div class="inner"><i class="fa fa-spinner fa-spin"></i> Načítam...</div>',
+          html: true
+        }
+      )
+    end
+  end
 end
